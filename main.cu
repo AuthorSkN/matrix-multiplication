8959e3b6c21c943f91e15efae7c22a0a655aa92b
Vladislav Gavrilov <B4B-eST-7cB-JPU>
2019-12-25 10:36:17 +0400

    Added writing test data to file

--- main.cu
+++ main.cu
@@ -1,4 +1,6 @@
 #include "iostream"
+#include <fstream>
+#include <string>
 
 #define BLOCK_SIZE  16          // submatrix size
 #define N           1024        // matrix size is N*N
@@ -24,8 +26,29 @@ __global__ void matMult ( float * a, float * b, int n, float * c )
     c [ic + n * ty + tx] = sum;
 }
 
+void write_data_to_file(std::string messge, float * a){
+    std::ofstream myfile;
+    myfile.open ("test_data.txt", std::fstream::app);
+    myfile << messge + "\n";
+    for ( int i = 0; i < N; i++ )
+        for ( int j = 0; j < N; j++ )
+        {
+            int	k = N*i + j;
+            myfile << a [k];
+        }
+    myfile << "\n";
+    myfile.close();
+}
+
+void clear_file(){
+    std::ofstream ofs;
+    ofs.open("test_data.txt", std::ofstream::out | std::ofstream::trunc);
+    ofs.close();
+}
+
 int main ( int argc, char *  argv [] )
 {
+    clear_file();
     int numBytes = N * N * sizeof ( float );
 
     // allocate host memory
@@ -41,6 +64,8 @@ int main ( int argc, char *  argv [] )
             a [k] = 0.0f;
             b [k] = 1.0f;
         }
+    write_data_to_file("First matrix", a);
+    write_data_to_file("Second matrix", b);
 
     // allocate device memory
     float * adev = NULL;
@@ -77,6 +102,7 @@ int main ( int argc, char *  argv [] )
 
     // print the cpu and gpu times
     printf("time spent executing by the GPU: %.2f millseconds\n", gpuTime );
+    write_data_to_file("Result matrix", c);
 
     // release resources
     cudaEventDestroy ( start );
